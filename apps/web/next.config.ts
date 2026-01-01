c01cb71f81bf94e7a08a3843dfc8c39c7d0022d3
Jing Chen <eonminion@gmail.com>
2026-01-01 03:19:51 -0500

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@ocean-dataview/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
